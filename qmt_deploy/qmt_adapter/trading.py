c81b7f16f2de1088e49384d1df4006e047055cd8
OWL <owl@zoo.dev>
2026-08-26 18:37:54 +0800

    Revert "fix(qmt): use our own buy prices instead of QMT m_dOpenPrice"
    This reverts commit 7d3bc94fb37ddfda76672bc7766dc4bf6f84f025.

--- qmt_deploy/qmt_adapter/trading.py
+++ qmt_deploy/qmt_adapter/trading.py
@@ -144,12 +144,11 @@ class QmtAccount(object):
             vol = getattr(p, 'm_nVolume', 0)
             if vol > 0:
                 code = getattr(p, 'm_strInstrumentID', '') + '.' + getattr(p, 'm_strExchangeID', '')
-                our_cost = self._buy_prices.get(code, getattr(p, 'm_dOpenPrice', 0))
                 holdings.append({
                     'code': code,
                     'shares': vol,
                     'available': getattr(p, 'm_nCanUseVolume', 0),
-                    'avg_cost': our_cost,
+                    'avg_cost': getattr(p, 'm_dOpenPrice', 0),
                     'name': getattr(p, 'm_strInstrumentName', ''),
                 })
 
